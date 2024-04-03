00b3dbfad76ce9b4edbdae845b05fc8ddfa215ca
Aaron Styles <aaron@charcoalstyles.com>
2024-04-03 22:05:11 +1100

    Some Text fixing, both in html and hte in-game title

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -17,6 +17,15 @@ class MenuState extends FlxState
 
 	var globalState:GlobalState;
 
+	var xpStaticOffset = 3; // 0
+	var xpDivisor = 17; // 1
+	var xpOffset = 25; // 2
+
+	var xpNegStaticOffset = 40; // 3
+	var xpNegMult = 2; // 4
+	var xpNegDivisor = 2; // 5
+	var xpNegOffset = 14; // 6
+
 	override public function create()
 	{
 		globalState = new GlobalState();
@@ -34,10 +43,14 @@ class MenuState extends FlxState
 			add(textArray[i]);
 		}
 
-		var instructions:FlxText = new FlxText(0, FlxG.height - 32, FlxG.width, "Press any key or button to start");
+		var instructions:FlxText = new FlxText(0, FlxG.height - 64, FlxG.width, "Press any key or button to start");
 		instructions.size = 16;
 		instructions.alignment = "center";
 		add(instructions);
+		var fullScrenInstruction:FlxText = new FlxText(0, FlxG.height - 32, FlxG.width, "Press 'F' to toggle full screen");
+		fullScrenInstruction.size = 16;
+		fullScrenInstruction.alignment = "center";
+		add(fullScrenInstruction);
 
 		el = 180;
 
@@ -83,19 +96,27 @@ class MenuState extends FlxState
 
 		// Create an array of FlxText objects
 		var textArray:Array<FlxText> = new Array<FlxText>();
+		var textArrayO:Array<FlxText> = new Array<FlxText>();
 
 		// Create a FlxText object for each character and position
 		// them centered on the screen
-		var acculumX = 0;
+		var acculumX:Float = 0;
 		for (i in 0...characters.length)
 		{
-			var n = characters[i] == "I";
-			var xpos = n ? 28 : 36;
-			var xpNeg = n ? 8 : 0;
+			var iChar = characters[i] == "I" || characters[i] == "i";
+			var mChar = characters[i] == "M" || characters[i] == "m";
+			var spaceChar = characters[i] == " ";
 
-			var char:FlxText = new FlxText(0, 0, FlxG.width, characters[i]);
-			char.size = 48;
-			char.alignment = "center";
+			var xposOld = iChar ? 30 : mChar ? 44 : 36;
+			var xpNegOld = iChar ? 6 : mChar ? -8 : 0;
+
+			// var c:FlxText = new FlxText(0, 0, -1, characters[i], 48);
+			// FlxG.log.add(characters[i] + " : " + c.width);
+
+			var char:FlxText = new FlxText(0, 0, -1, characters[i], 48);
+			var xpos = spaceChar ? 36 : ((char.width + xpStaticOffset) / xpDivisor + xpOffset); // char.width - 4;
+			var xpNeg = spaceChar ? 0 : (xpNegStaticOffset - (char.width * xpNegMult)) / xpNegDivisor + xpNegOffset; // 40 - char.width;
+
 			char.x = acculumX + xpos;
 			char.y = 0;
 			char.borderColor = 0xffffffff;
@@ -106,19 +127,25 @@ class MenuState extends FlxState
 			acculumX += xpos - xpNeg;
 		}
 
+		// get the left most start position of the title, using flxG.width and acculumX
+		var titleStart = (FlxG.width - acculumX) / 2;
+
 		for (i in 0...textArray.length)
 		{
-			textArray[i].x -= acculumX / 2;
+			textArray[i].x += titleStart;
 		}
 
 		return textArray;
 	}
+
+	var ac:Int = 0;
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
 		el += elapsed;
+
 		for (i in 0...textArray.length)
 		{
 			textArray[i].y = 64 + Math.sin(el * 2.3 + i * 0.5) * 15;
@@ -138,19 +165,24 @@ class MenuState extends FlxState
 			FlxG.switchState(new PlayState());
 		}
 
-		if (FlxG.keys.anyJustPressed(generateAllKeys()))
+		// if (FlxG.keys.anyJustPressed(generateAllKeys()))
+		// {
+		// 	globalState.isUsingController = false;
+		// 	FlxG.switchState(new PlayState());
+		// }
+
+		if (FlxG.keys.anyJustPressed([F]))
 		{
-			globalState.isUsingController = false;
-			FlxG.switchState(new PlayState());
+			FlxG.fullscreen = !FlxG.fullscreen;
 		}
 	}
 
 	function generateAllKeys():Array<flixel.input.keyboard.FlxKey>
 	{
 		return [
-			A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
-			NUMPADZERO, NUMPADONE, NUMPADTWO, NUMPADTHREE, NUMPADFOUR, NUMPADFIVE, NUMPADSIX, NUMPADSEVEN, NUMPADEIGHT, NUMPADNINE, F1, F2, F3, F4, F5, F6,
-			F7, F8, F9, F10, F11, F12, TAB, CAPSLOCK, SHIFT, CONTROL, ALT, SPACE, ENTER, BACKSPACE, DELETE, INSERT, HOME, END, PAGEUP, PAGEDOWN, PRINTSCREEN,
+			A, B, C, D, E, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, NUMPADZERO,
+			NUMPADONE, NUMPADTWO, NUMPADTHREE, NUMPADFOUR, NUMPADFIVE, NUMPADSIX, NUMPADSEVEN, NUMPADEIGHT, NUMPADNINE, F1, F2, F3, F4, F5, F6, F7, F8, F9,
+			F10, F11, F12, TAB, CAPSLOCK, SHIFT, CONTROL, ALT, SPACE, ENTER, BACKSPACE, DELETE, INSERT, HOME, END, PAGEUP, PAGEDOWN, PRINTSCREEN,
 		];
 	}
 }
